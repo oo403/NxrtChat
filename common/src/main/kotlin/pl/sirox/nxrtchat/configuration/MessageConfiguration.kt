646354dc83961c8489cf73acbf39aa5e909dfb12
403 <oo403.kontakt@gmail.com>
2026-04-16 19:31:01 +0200

    feat: last message not found message if map is empty or player is offline

--- common/src/main/kotlin/pl/sirox/nxrtchat/configuration/MessageConfiguration.kt
+++ common/src/main/kotlin/pl/sirox/nxrtchat/configuration/MessageConfiguration.kt
@@ -6,6 +6,7 @@ class MessageConfiguration : OkaeriConfig() {
 
     var invalidUsage: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Invalid usage!"
     var missingPermission: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Missing permission! ( <permissions> )"
+    var lastMessageNotFound: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Last message not found!"
 
     var msg: MutableMap<String, Msg> = mutableMapOf(
         "sender" to Msg().apply {
--- paper/src/main/kotlin/pl/sirox/nxrtchat/command/commands/user/ReplyCommand.kt
+++ paper/src/main/kotlin/pl/sirox/nxrtchat/command/commands/user/ReplyCommand.kt
@@ -5,24 +5,38 @@ import dev.rollczi.litecommands.annotations.command.Command
 import dev.rollczi.litecommands.annotations.context.Context
 import dev.rollczi.litecommands.annotations.execute.Execute
 import dev.rollczi.litecommands.annotations.join.Join
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import pl.sirox.nxrtchat.command.CustomCommand
+import pl.sirox.nxrtchat.configuration.MessageConfiguration
 import pl.sirox.nxrtchat.service.MessageService
 import pl.sirox.nxrtchat.service.PlayerService
 
 @Command(name = "reply", aliases = ["r"])
 class ReplyCommand @Inject constructor(
     private val playerService: PlayerService,
-    private val messageService: MessageService
+    private val messageService: MessageService,
+    private val messageConfiguration: MessageConfiguration
 ) : CustomCommand {
 
     @Execute
     fun executeReply(@Context sender: Player, @Join("message") message: String) {
-        val targetUUID = playerService.getLastMsg(sender.uniqueId) ?: return
-        val target = Bukkit.getPlayer(targetUUID) ?: return
+        val lastMessageNotFoundMessage = MiniMessage.miniMessage().deserialize(messageConfiguration.lastMessageNotFound)
 
-        messageService.sendMsg(sender, target, message)
+        val targetUUID = playerService.getLastMsg(sender.uniqueId)
+
+        if (targetUUID == null) {
+            sender.sendMessage(lastMessageNotFoundMessage)
+        } else {
+            val target = Bukkit.getPlayer(targetUUID)
+
+            if (target == null) {
+                sender.sendMessage(lastMessageNotFoundMessage)
+            } else {
+                messageService.sendMsg(sender, target, message)
+            }
+        }
     }
 
 }
