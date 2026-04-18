54583f4dc5d63216a3955142a21f1c0522c6c4aa
403 <oo403.kontakt@gmail.com>
2026-04-19 00:32:05 +0200

    style: added config comments

--- common/src/main/kotlin/pl/sirox/nxrtchat/configuration/GeneralConfiguration.kt
+++ common/src/main/kotlin/pl/sirox/nxrtchat/configuration/GeneralConfiguration.kt
@@ -1,13 +1,17 @@
 package pl.sirox.nxrtchat.configuration
 
 import eu.okaeri.configs.OkaeriConfig
+import eu.okaeri.configs.annotation.Comment
 
 class GeneralConfiguration : OkaeriConfig() {
 
+    @Comment("Format to use for messages","Available formats: global, ranks")
     var format: String = "global"
 
+    @Comment("","Format used for all players, regardless of rank")
     var globalFormat: String = "<player> » <message>"
 
+    @Comment("","Format used for players with specific rank")
     var ranksFormat: MutableMap<String, RanksFormat> = mutableMapOf(
         "default" to RanksFormat().apply {
             format = "<player> » <message>"
@@ -18,8 +22,10 @@ class GeneralConfiguration : OkaeriConfig() {
         var format: String = "<player> » <message>"
     }
 
+    @Comment("","Sound to play when someone mentions you")
     var mentionSound: String = "minecraft:entity.player.levelup"
 
+    @Comment("","Format to use for mentions")
     var mentionFormat: String = "<green>@<player>"
 
 }
--- common/src/main/kotlin/pl/sirox/nxrtchat/configuration/MessageConfiguration.kt
+++ common/src/main/kotlin/pl/sirox/nxrtchat/configuration/MessageConfiguration.kt
@@ -1,16 +1,24 @@
 package pl.sirox.nxrtchat.configuration
 
 import eu.okaeri.configs.OkaeriConfig
+import eu.okaeri.configs.annotation.Comment
 
 class MessageConfiguration : OkaeriConfig() {
 
+    @Comment("Invalid usage message")
     var invalidUsage: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Invalid usage!"
+    @Comment("","Missing permission message", "Available placeholders: <permissions>")
     var missingPermission: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Missing permission! ( <permissions> )"
+    @Comment("","Player not found message")
     var playerNotFound: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Player not found!"
+    @Comment("","Last message not found message")
     var lastMessageNotFound: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Last message not found!"
+    @Comment("","Config reload success message")
     var configReloadSuccess: String = "<#AAAAAA>[<#5DF083>✔<#AAAAAA>] Config reloaded successfully!"
+    @Comment("","Config reload failed message")
     var configReloadFailed: String = "<#AAAAAA>[<#F03C3C>✘<#AAAAAA>] Config reload failed!"
 
+    @Comment("","Msg format")
     var msg: MutableMap<String, Msg> = mutableMapOf(
         "sender" to Msg().apply {
             format = "You » <target>: <message>"
